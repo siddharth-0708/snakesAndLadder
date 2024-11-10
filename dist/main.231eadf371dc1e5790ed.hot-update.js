17cbe36a58f288676b81b81180ea0e72547d1f40
siddharth-0708 <siddharth0708@gmail.com>
2024-11-10 10:29:02 +0530

    ss

---
+++ dist/main.231eadf371dc1e5790ed.hot-update.js
@@ -0,0 +1,30 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatesnakesandladder"]("main",{
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dummy_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dummy.js */ \"./dummy.js\");\n/* harmony import */ var react_dom_client__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom/client */ \"./node_modules/react-dom/client.js\");\n/* harmony import */ var _FirstComponent__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./FirstComponent */ \"./FirstComponent.jsx\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ \"./node_modules/react/jsx-runtime.js\");\n\n\n\n\nconsole.log((0,_dummy_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])());\nvar root = react_dom_client__WEBPACK_IMPORTED_MODULE_1__.createRoot(document.getElementById('root'));\nroot.render(/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_FirstComponent__WEBPACK_IMPORTED_MODULE_2__[\"default\"], {}));\n\n//# sourceURL=webpack://snakesandladder/./index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("54d11e26fcbc5914bf31")
+/******/ })();
+/******/ 
+/******/ }
+);
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import sid from "./dummy.js";
 import ReactDOM from 'react-dom/client';
 import FirstComponent from "./FirstComponent";
-import { data } from "autoprefixer";
 
 console.log(sid());
 const root = ReactDOM.createRoot(document.getElementById('root'));
