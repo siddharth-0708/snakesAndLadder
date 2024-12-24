6625cee980dad355407921d406d627b782abbec6
siddharth-0708 <siddharth0708@gmail.com>
2024-12-24 22:27:05 +0530

    game changes

--- src/board/innerBoard/InnerBoard.tsx
+++ src/board/innerBoard/InnerBoard.tsx
@@ -1,6 +1,9 @@
 import styles from './InnerBoard.module.css';
-import { useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 
+type cellProps = {
+    element: number;
+}
 function InnerBoard(){
     const [elementsArray, setElementsArray] = useState<number[]>([]);
 
@@ -14,9 +17,25 @@ function InnerBoard(){
     return (
             <div className={styles.innerParent}>
                 {elementsArray?.map(
-                    (ele) => <div className= {styles.cell} style={{ gridArea: `cell${ele}` }} key={ele}> {ele}</div>)
+                    (ele) => <Cell element = {ele} key={ele}/>)
                 }
+            <div className={styles.icon}>icon</div>    
             </div>
     )
 }
 export default InnerBoard;
+
+export const Cell: FC<cellProps> = ({element}) => {
+    const cellRef = useRef<HTMLDivElement>(null);
+
+    useEffect(()=>{
+        if(cellRef.current){
+            console.log("...bottom is ", element, " ", cellRef.current.getBoundingClientRect().bottom);
+            console.log("...bottom is offset top", element, " ", cellRef.current.offsetTop);
+            console.log("...bottom is offset left", element, " ", cellRef.current.offsetLeft);
+        }
+    }, [cellRef]);
+    return (
+        <div ref = {cellRef} className= {styles.cell} style={{ gridArea: `cell${element}` }}> {element}</div>
+    )
+}
