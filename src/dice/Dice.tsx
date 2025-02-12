be83ec4dfdffb2a5b6e000ed2ded6e8c80e1dc51
siddharth-0708 <siddharth0708@gmail.com>
2025-02-12 08:07:13 +0530

    dice changes

--- src/dice/Dice.tsx
+++ src/dice/Dice.tsx
@@ -1,9 +1,12 @@
 import styles from './Dice.module.css';
+import { useDispatch } from 'react-redux';
 import { snakesAndLadderActions } from '../store/gameSlices';
 
 function Dice(){
+    const dispatch = useDispatch();
+
     function generateRandomNumber(){
-        snakesAndLadderActions.setDiceData(Math.floor(Math.random() * 6) + 1);
+        dispatch(snakesAndLadderActions.setDiceData(Math.floor(Math.random() * 6) + 1));
         // return Math.floor(Math.random() * 6) + 1;
     }
     function rollDice(){
